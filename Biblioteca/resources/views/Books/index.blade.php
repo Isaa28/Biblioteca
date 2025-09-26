3b474b09ffd25d76d5930375129316a0589b2c1c
Isaa28 <isadora.silva112@aluno.ce.gov.br>
2025-09-26 09:04:06 -0300

    Criação do arquivo index.

---
+++ Biblioteca/resources/views/Books/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca</title>
+</head>
+<body>
+    <h1>Livros cadastrados</h1>
+    <a href="{{route('books.create')}}">Cadastrar livro</a>
+    <hr>
+    <table>
+        <tr>
+            <th>Nome</th>
+            <th>Autor</th>
+            <th>Quantidade</th>
+        </tr>
+        @foreach ($books as $book)
+            <tr>
+                <td>{{$book->name}}</td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+</html>
