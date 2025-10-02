14431ef81c80c9322271ca0bbdb9fcfb713220b3
Isa <isadora.silva112@aluno.ce.gov.br>
2025-10-02 13:02:41 -0300

    estilização das telas com formulário.

--- Biblioteca/resources/views/Books/create.blade.php
+++ Biblioteca/resources/views/Books/create.blade.php
@@ -3,19 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/form.css') }}">
     <title>Cadastrar</title>
 </head>
 <body>
-    <h2>Cadastrar livro</h2>
-    <form action="{{route('books.store')}}" method="post">
-        @csrf
-        <label for="name">Nome:</label>
-        <input type="text" name="name">
-        <label for="author">Autor:</label>
-        <input type="text" name="author">
-        <label for="amount">Quantidade:</label>
-        <input type="number" name="amount" id="">
-        <input type="submit" value="Cadastrar">
-    </form>
+    <div id="caixaDoFormulario">
+        <h2 id="titulo">Cadastrar livro</h2>
+        <form action="{{route('books.store')}}" method="post" id="formulario">
+            @csrf
+            <div class="gruposDoFormulario">
+                <label for="name">Nome:</label>
+                <input type="text" name="name">
+            </div>
+            <div class="gruposDoFormulario">
+                <label for="author">Autor:</label>
+                <input type="text" name="author">
+            </div>
+            <div class="gruposDoFormulario">
+            <label for="amount">Quantidade:</label>
+            <input type="number" name="amount" id="">
+            </div>
+            <input type="submit" id="botao" value="Cadastrar">
+        </form>
+    </div>
 </body>
 </html>
--- Biblioteca/resources/views/Books/edit.blade.php
+++ Biblioteca/resources/views/Books/edit.blade.php
@@ -3,20 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/form.css') }}">
     <title>Editar</title>
 </head>
 <body>
-    <h2>Editar livro</h2>
-    <form action="{{route('books.update', $book->id)}}" method="post">
-        @csrf
-        @method('PUT')
-        <label for="name">Nome:</label>
-        <input type="text" name="name" value="{{ $book->name }}">
-        <label for="author">Autor:</label>
-        <input type="text" name="author" value="{{ $book->author }}">
-        <label for="amount">Quantidade:</label>
-        <input type="number" name="amount" id="" value="{{ $book->amount }}">
-        <input type="submit" value="Editar">
-    </form>
+    <div id="caixaDoFormulario">
+        <h2 id="titulo">Editar livro</h2>
+        <form action="{{route('books.update', $book->id)}}" method="post" id="formulario">
+            @csrf
+            @method('PUT')
+            <div class="gruposDoFormulario">
+            <label for="name">Nome:</label>
+            <input type="text" name="name" value="{{ $book->name }}">
+            </div>
+            <div class="gruposDoFormulario">
+            <label for="author">Autor:</label>
+            <input type="text" name="author" value="{{ $book->author }}">
+            </div>
+            <div class="gruposDoFormulario">
+            <label for="amount">Quantidade:</label>
+            <input type="number" name="amount" id="" value="{{ $book->amount }}">
+            </div>
+            <input type="submit" id="botao" value="Editar">
+        </form>
+    </div>
 </body>
 </html>
