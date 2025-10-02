4c7576a2292ab94d6d46f1e5c7f3d5dfdf60a561
Isa <isadora.silva112@aluno.ce.gov.br>
2025-10-02 13:22:01 -0300

    Finalização do css.

--- Biblioteca/resources/views/Books/show.blade.php
+++ Biblioteca/resources/views/Books/show.blade.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/show.css') }}">
     <title>{{$book->name}}</title>
 </head>
 <body>
-    <h1>{{$book->name}}</h1>
-    <h3>Autor</h3>
-    <p>{{$book->author}}</p>
-    <h3>Quantidade</h3>
-    <p>{{$book->amount}}</p>
+    <div id="detalhesLivro">
+        <h1 id="titulo">{{$book->name}}</h1>
+        <h3 class="tituloDaInfor">Autor</h3>
+        <p>{{$book->author}}</p>
+        <h3 class="tituloDaInfor">Quantidade</h3>
+        <p>{{$book->amount}}</p>
+        <a href="{{ route('books.index') }}" id="botao">Voltar</a>
+    </div>
 </body>
 </html>
