a6e49f815e5d246bbb46be078b0fcddd784048f7
Isa <isadora.silva112@aluno.ce.gov.br>
2025-10-02 12:40:47 -0300

    Estilização da listagem

--- Biblioteca/resources/views/Books/index.blade.php
+++ Biblioteca/resources/views/Books/index.blade.php
@@ -4,21 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca</title>
+    <link rel="stylesheet" href="{{ asset('css/lista.css') }}">
+    <script src="https://kit.fontawesome.com/2e3161fd5b.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Livros cadastrados</h1>
-    <a href="{{route('books.create')}}">Cadastrar livro</a>
-    <hr>
+    <div id="cabeçalho">
+        <h1 id="titulo">Livros cadastrados</h1>
+        <a id="botaoCadastrar" href="{{route('books.create')}}">Cadastrar livro</a>
+        <hr>
+    </div>
     <table>
         <tr>
-            <th>Nome</th>
+            <th id="tituloTabela">NOME</th>
         </tr>
         @foreach ($books as $book)
-            <tr>
+            <tr id="linhasdatabela">
                 <td>{{$book->name}}</td>
-                <td><a href="{{route('books.edit', $book->id)}}">✏️</a></td>
-                <td><form action="{{route('books.destroy', $book->id)}}" method="post"> @csrf @method('DELETE') <input type="submit" value="🗑️"></form></td>
-                <td><a href="{{route('books.show', $book->id)}}">Show</a></td>
+                <td><a href="{{route('books.edit', $book->id)}}"><i class="fa-solid fa-pen"></i></a></td>
+                <td><form action="{{route('books.destroy', $book->id)}}" method="post"> @csrf @method('DELETE') <button type="submit"><i class="fa-regular fa-trash-can"></i></button></form></td>
+                <td><a href="{{route('books.show', $book->id)}}"><i class="fa-regular fa-eye"></i></a></td>
             </tr>
         @endforeach
     </table>
